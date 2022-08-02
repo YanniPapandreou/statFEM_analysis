1902350eda66dc9a2c3a3faf6ea35b7c234beadb
YanniPapandreou <nepo1y@gmail.com>
2022-08-02 19:32:21 +0300

    started configuring docs

---
+++ docs/source/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'statFEM_analysis'
+copyright = '2022, Yanni Papandreou'
+author = 'Yanni Papandreou'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../statFEM_analysis'))
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.mathjax',
+]
+
+# autodoc_mock_imports = ["dolfin", "numpy", "scipy", "joblib", "multiprocessing"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,9 @@
+Welcome to statFEM_analysis's documentation!
+============================================
+
+.. toctree:: 
+   :maxdepth: 2
+
+   usage
+
+

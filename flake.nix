61412fede8418e00ef8cd1d1effc9e79239df13d
Yanni Papandreou <nepo1y@gmail.com>
2025-02-20 00:01:44 +0000

    started porting code to newer package versions

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Python dev environment with dependencies managed by Nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      pythonEnv = pkgs.python313.withPackages (
+        ps: with ps; [
+          fenics
+          ipython
+          joblib
+          jupyterlab
+          nbdev
+          numba
+          pot
+          pynvim
+          rich
+          seaborn
+          tqdm
+        ]
+      );
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [ pythonEnv ];
+        shellHook = ''
+          # Append src directory to PYTHONPATH for development
+          export PYTHONPATH="$PYTHONPATH:$PWD/src"
+          echo 'Python development environment for statFEM activated'
+        '';
+      };
+    };
+}

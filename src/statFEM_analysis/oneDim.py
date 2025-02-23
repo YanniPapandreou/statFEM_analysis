1c195aa7e2e4525c3364ae3b43b44fbea5d7c4ff
Yanni Papandreou <nepo1y@gmail.com>
2025-02-23 23:01:23 +0000

    updated code and ran most notebooks

--- src/statFEM_analysis/oneDim.py
+++ src/statFEM_analysis/oneDim.py
@@ -252,6 +252,7 @@ def cov_assembler(J, k_f, grid, parallel, translation_inv):
     A = assemble(a)
     M = as_backend_type(M).sparray()
     A = as_backend_type(A).sparray()
+    # TODO: remove this commented code
     # M = csr_matrix(M.getValuesCSR()[::-1], shape=M.size)
     # A = csr_matrix(A.getValuesCSR()[::-1], shape=A.size)
 
@@ -509,6 +510,7 @@ def fem_cov_assembler_post(J, k_f, Y, parallel, translation_inv):
     A = assemble(a)
     M = as_backend_type(M).sparray()
     A = as_backend_type(A).sparray()
+    # TODO: remove this commented code
     # M = csr_matrix(M.getValuesCSR()[::-1], shape=M.size)
     # A = csr_matrix(A.getValuesCSR()[::-1], shape=A.size)
 
@@ -721,6 +723,7 @@ def post_fem_cov_assembler(J, k_f, grid, Y, parallel, translation_inv):
     A = assemble(a)
     M = as_backend_type(M).sparray()
     A = as_backend_type(A).sparray()
+    # TODO: remove this commented code
     # M = csr_matrix(M.getValuesCSR()[::-1], shape=M.size)
     # A = csr_matrix(A.getValuesCSR()[::-1], shape=A.size)
 
